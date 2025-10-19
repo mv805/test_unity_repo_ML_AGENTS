aade668085813846f792b03d7d6424658a68f032
Matt V <97783221+mv805@users.noreply.github.com>
2025-10-19 06:33:02 -0700

    Add Obstacle script and remove dummy script
    - Created a new Obstacle.cs script that handles obstacle behavior including random size, speed, and rotation.
    - Removed the dummy.cs script as it was no longer needed.
    - Updated EditorSettings to enable play mode options.
    - Added TimelineSettings asset for timeline configuration.

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float minSize = 0.5f;
+    public float maxSize = 2.0f;
+    public float minSpeed = 100f;
+    public float maxSpeed = 300f;
+    public float maxSpinSpeed = 20f;
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float randomSize = Random.Range(minSize, maxSize);
+        transform.localScale = new Vector3(randomSize, randomSize, 1);
+
+        float randomSpeed = Random.Range(minSpeed, maxSpeed) / randomSize;
+        Vector2 randomDirection = Random.insideUnitCircle;
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(randomDirection * randomSpeed);
+
+        float randomTorque = Random.Range(-maxSpinSpeed, maxSpinSpeed);
+        rb.AddTorque(randomTorque);
+    }
+
+    // Update is called once per frame
+    void Update() { }
+}
